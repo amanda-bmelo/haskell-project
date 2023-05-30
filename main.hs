4af84c5e52677e5165f384d3a7c263b10ec83c45
AMANDA MELO <ammelo@id.uff.br>
2023-05-30 20:41:35 -0300

    Estados Faltando ok

--- main.hs
+++ main.hs
@@ -1,41 +1,49 @@
 import Data.List (nub)
 
-type State = String
-type Label = String
-type Edge = (State, Label, State)
-type Frame = ([State], [Edge])
+type Estado = String
+type Rotulo = String
+type Aresta = (Estado, Rotulo, Estado)
+type Frame = ([Estado], [Aresta])
 
-type Program = [Label]
+type Programa = [Rotulo]
 
-checkFrameValidity :: Frame -> Program -> Either String ()
-checkFrameValidity (states, edges) program = do
-  checkStates states
-  checkEdges edges
-  checkProgram program edges
+checkFrameValidade :: Frame -> Programa -> Either String ()
+checkFrameValidade (estados, arestas) programa = do
+  checkEstados estados
+  checkArestas arestas
+  checkEstadosFaltando estados arestas
+  checkPrograma programa arestas
 
-checkStates :: [State] -> Either String ()
-checkStates states =
-  if length states /= length (nub states)
-    then Left "O conjunto de estados contém elementos duplicados."
+checkEstados :: [Estado] -> Either String ()
+checkEstados estados =
+  if length estados /= length (nub estados)
+    then Left "O conjunto de estados contém estados duplicados."
     else Right ()
 
-checkEdges :: [Edge] -> Either String ()
-checkEdges edges =
-  if length edges /= length (nub edges)
-    then Left "O conjunto de arestas contém elementos duplicados."
+checkArestas :: [Aresta] -> Either String ()
+checkArestas arestas =
+  if length arestas /= length (nub arestas)
+    then Left "O conjunto de arestas contém arestas duplicadas."
     else Right ()
 
-checkProgram :: Program -> [Edge] -> Either String ()
-checkProgram program edges =
-  case filter (\(from, label, to) -> label `notElem` program) edges of
+checkPrograma :: Programa -> [Aresta] -> Either String ()
+checkPrograma programa arestas = do
+  case filter (\(_, rotulo, _) -> rotulo `notElem` programa) arestas of
     [] -> Right ()
-    invalidEdges -> Left $ "As seguintes arestas não estão presentes no programa: " ++ show invalidEdges
+    arestasInvalidas -> Left $ "As seguintes arestas não estão presentes no programa: " ++ show arestasInvalidas
+
+checkEstadosFaltando :: [Estado] -> [Aresta] -> Either String ()
+checkEstadosFaltando estados arestas = do
+  case filter (\(origem, _, destino) -> origem `notElem` estados || destino `notElem` estados) arestas of
+    [] -> Right ()
+    estadosFaltando -> Left $ "As seguintes arestas contêm estados não presentes na lista de estados: " ++ show estadosFaltando
+
 
 -- Exemplo básico
 main :: IO ()
 main = do
-  let frame = (["A", "B", "C"], [("A", "label1", "B"), ("B", "label2", "C")])
-      program = ["label1", "label2"]
-  case checkFrameValidity frame program of
+  let frame = (["A", "B", "C"], [("A", "passo1", "B"), ("B", "passo2", "C")])
+      programa = ["passo1", "passo2"]
+  case checkFrameValidade frame programa of
     Right _ -> putStrLn "O frame é válido para o programa."
     Left errMsg -> putStrLn $ "O frame não é válido para o programa. Erro: " ++ errMsg
