e170b5182a5293cf196b73490737e9a4744cd579
AMANDA MELO <ammelo@id.uff.br>
2023-05-30 17:22:12 -0300

    inputs

---
+++ main.hs
@@ -0,0 +1,8 @@
+main = do  
+    putStrLn "Considere um frame PDL F = (W, R)."  
+    putStrLn "Diga quais são os vértices do grafo:"  
+    vertices <- getLine  
+    putStrLn "Diga quais são as arestas do grafo:"  
+    arestas <- getLine  
+    putStrLn (vertices)
+    putStrLn (arestas)
